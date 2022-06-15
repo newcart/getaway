6f1cc8dac5879d7fa39e93726d218bb36872f425
tulparstudyo <teknovipyazilim@gmail.com>
2022-06-15 16:22:57 +0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,12 @@ import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TrendyolModule } from './rooter/trendyol/trendyol.module';
+import { PazaramaModule } from './rooter/pazarama/pazarama.module';
 import { authMiddleware } from "./auth/auth.middleware";
 import { AuthService } from "./auth/auth.basic.service";
 
 @Module({
-  imports: [ TrendyolModule],
+  imports: [ TrendyolModule, PazaramaModule],
   controllers: [AppController],
   providers: [AuthService, AppService],
 })
